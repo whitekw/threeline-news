bb3c9739e4819d378c4ab1a32b1aef91b4cf0d13
kyeongwon <kyeongwon329@gmail.com>
2024-10-01 22:34:29 +0900

    기사 상세페이지

--- src/app/(route)/article/[id]/page.tsx
+++ src/app/(route)/article/[id]/page.tsx
@@ -12,8 +12,23 @@ const ArticlePage = async ({params}: {params: {id: number}}) => {
 
   return (
     <MainContent>
-      <h1 className={styles.title}>{data.title}</h1>
+      <div className={styles.header}>
+        <h1 className={styles.title}>{data.title}</h1>
+        <div className={styles.date}>작성일 : 2024.09.30 15:30</div>
+      </div>
       <img className={styles.image} src={data.image_url}/>
+
+      <ol className={styles.newsContent}>
+        <li>{data.content.split("\n")[0]}</li>
+        <li>{data.content.split("\n")[1]}</li>
+        <li>{data.content.split("\n")[2]}</li>
+      </ol>
+
+      <div className={styles.menu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </MainContent>
   );
 };
